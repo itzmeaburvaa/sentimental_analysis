8f1ca96f93ca09748cb94fab5e0d1b348fcb0110
itzmeaburvaa <aburvaasenthilkumarias@gmail.com>
2026-05-09 23:04:25 +0530

    first commit 2

--- app.py
+++ app.py
@@ -79,6 +79,7 @@ def predict():
     )
 
 
+import os
+
 if __name__ == "__main__":
-    # Run the app in debug mode for local development.
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
